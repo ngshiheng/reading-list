702201771cd05a7eb0bf2c80dc90adabc09b7325
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-09-20 16:22:09 +0800

    Added apollo client to query data with GraphQL backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 // Components
 import BookList from './components/BookList';
 
-class App extends Component{
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+});
+
+class App extends Component {
   render() {
     return (
+    <ApolloProvider client={client}>
       <div id="main">
         <h1>Jerry's Reading List</h1>
         <BookList/>
       </div>
+    </ApolloProvider>
     )
   }
 }
