88f0d0bbebad410bf6389080630cfaf570c240a9
jerryng <Jerry Ng>
2019-11-09 11:33:47 +0800

    Script for quick setup for Linux users

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#################################################################################
+# Run this script for development.
+# If you only want to see the finished product, use Docker. 
+# Docker instruction: [https://github.com/ngshiheng/reading-list#using-docker].
+#################################################################################
+
+# font color
+green=`tput setaf 2`
+white=`tput sgr0`
+
+# functions
+npm_install () {
+    (cd $1 && npm i)
+    echo "$green✓$white Running npm install at $1"
+}
+
+echo_finish () {
+    echo "$green✓$white Finished setup."
+}
+
+# main
+npm_install ./server
+npm_install ./client
+echo_finish
